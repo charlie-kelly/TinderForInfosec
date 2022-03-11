5b38a64e55f103ec3d9256410865a3b32fc4514d
Charlie Kelly <charlie.kelly@outlook.com>
2022-03-11 00:18:34 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,250 @@
+(function () {
+  const items = [
+    {
+      id: 0,
+      name: 'Log4Shell',
+      age: 1,
+      job: 'Goes by CVE-2021-44228',
+      images: ['images/testpic5.jpg', 'images/testpic2.jpg', 'images/testpic1.jpg'],
+      distance: 25,
+      description: 'Swipe for me! You’ll find me very ap-peel-ing'
+    },
+    {
+      id: 1,
+      name: 'Conti',
+      age: 28,
+      job: 'Saint Petersburg, Russia',
+      images: ['images/conti.png'],
+      distance: 200,
+      description: 'Do you like encryption? We got ya'
+    },
+    {
+      id: 2,
+      name: 'NCC Group',
+      age: 23,
+      job: 'Manchester',
+      images: ['images/ncc.jpg'],
+      distance: 9,
+      description: 'Big fan of CREST exams'
+    },
+    {
+      id: 3,
+      name: 'John McAfee',
+      age: 57,
+      job: 'Cocaine enthusiast',
+      images: ['images/mcafee.jpg'],
+      distance: 2455,
+      description: 'Good luck uninstalling McAfee'
+    },
+    {
+      id: 4,
+      name: 'Windows XP',
+      age: 21,
+      job: 'Supporting most CNI',
+      images: ['images/xp.jpg'],
+      distance: 2455,
+      description: 'Patch me daddy'
+    },
+    {
+      id: 5,
+      name: 'Cobalt Strike',
+      age: 29,
+      job: 'Professional Pen Tester',
+      images: ['images/cobaltstrike.png'],
+      distance: 1,
+      description: 'Definitely only used for legitimate reasons'
+      },
+    {
+      id: 6,
+      name: 'Limewire',
+      age: 36,
+      job: 'Linkin-Park-Numb.mp3.xlsm.exe',
+      images: ['images/limewire.png'],
+      distance: 14,
+      description: 'I hope you like multiple browser toolbars with your Eminem album',
+     },
+    {
+      id: 7,
+      name: 'Solarwinds',
+      age: 22,
+      job: 'Nearly bankrupt',
+      images: ['images/solarwinds.jpg'],
+      distance: 2,
+      description: 'Please buy me 🥺',
+    }
+  ];
+
+  const dataProvider = (function* () {
+    while (true) {
+      yield *items;
+    }
+  })();
+
+  function adjustSwipeItems() {
+    const top = document.querySelector('.item--top');
+    const next = document.querySelector('.item--next');
+    next.classList.add('hidden');
+    top.style.cssText = '';
+    top.style.position = 'relative';
+    const topR = top.getBoundingClientRect();
+    top.style.position = '';
+    next.classList.remove('hidden');
+    top.style.top = next.style.top = `${topR.top}px`;
+    top.style.width = next.style.width = `${topR.width}px`;
+    top.style.height = next.style.height = `${topR.height}px`;
+    top.onResize();
+    next.onResize();
+  }
+
+  function updateCards(event) {
+    const top = document.querySelector('.item--top');
+    window.ga && ga('send', 'event', `item-${top.data.id}`, event.detail);
+    const next = document.querySelector('.item--next');
+    const details = document.querySelector('tinderforbananas-details');
+    top.style.transform = '';
+    top.selected = 0;
+    top.data = next.data;
+    next.data = dataProvider.next().value;
+  }
+
+  function hookupButtons() {
+    const details = document.querySelector('.view--details');
+    document.querySelectorAll('.control--like').forEach(btn => 
+      btn.addEventListener('click', _ => {
+        let p = Promise.resolve();
+        if (!details.classList.contains('hidden')) {
+          p = hideDetails();
+        }
+        p.then(_ => document.querySelector('.item--top').like());
+      })
+    );
+    document.querySelectorAll('.control--nope').forEach(btn =>
+      btn.addEventListener('click', _ => {
+        let p = Promise.resolve();
+        if (!details.classList.contains('hidden')) {
+          p = hideDetails();
+        }
+        p.then(_ => document.querySelector('.item--top').nope());
+      })
+    );
+    document.querySelectorAll('.control--superlike').forEach(btn => 
+      btn.addEventListener('click', _ => {
+        let p = Promise.resolve();
+        if (!details.classList.contains('hidden')) {
+          p = hideDetails();
+        }
+        p.then(_ => document.querySelector('.item--top').superlike());
+      })
+    );
+  }
+
+  function showDetails(event) {
+    const swipelist = document.querySelector('.view--swipelist');
+    const data = swipelist.querySelector('.item--top').data;
+    window.ga && ga('send', 'event', `item-${data.id}`, 'details');
+    const details = document.querySelector('.view--details');
+    const detailsText1 = details.querySelector('.item__details');
+    const detailsText2 = details.querySelector('.description');
+    const detailsNav = details.querySelector('nav');
+    const carousel = document.querySelector('tinderforbananas-carousel');
+    const image = document.querySelector('.view--swipelist .item--top picture');
+    details.querySelector('tinderforbananas-details').data = data;
+
+    // Let’s do FLIP!
+    const start = image.getBoundingClientRect();
+    
+    swipelist.classList.add('overlaid');
+    details.classList.remove('hidden');
+
+    const target = carousel.getBoundingClientRect();
+    carousel.style.transformOrigin = 'top left';
+    carousel.style.transform = `scaleX(${start.width/target.width}) scaleY(${start.height/target.height}) translate(${start.left - target.left}px, ${start.top - target.top}px)`;   
+    return requestAnimationFramePromise()
+      .then(_ => requestAnimationFramePromise())
+      .then(_ => {
+        carousel.style.transition = 'transform 0.15s ease-in-out';
+        carousel.style.transform = 'initial';
+        detailsText1.style.transform = 'initial';
+        detailsText2.style.transform = 'initial';
+        detailsNav.style.transform = 'initial';
+        return transitionEndPromise(carousel);
+      })
+      .then(_ => {
+        carousel.style.transform = '';
+        carousel.style.transition = '';
+        carousel.style.transformOrigin = '';
+      });
+
+  }
+
+  function hideDetails(event) {
+    const swipelist = document.querySelector('.view--swipelist');
+    const details = document.querySelector('.view--details');
+    const detailsText1 = details.querySelector('.item__details');
+    const detailsText2 = details.querySelector('.description');
+    const detailsNav = details.querySelector('nav');
+    const carousel = document.querySelector('tinderforbananas-carousel');
+    const item = document.querySelector('.view--swipelist .item--top');
+    const image = document.querySelector('.view--swipelist .item--top picture');
+
+    item.selected = event && event.detail.selected || 0;
+
+    const start = carousel.getBoundingClientRect();
+    swipelist.classList.remove('overlaid');
+    details.classList.add('hidden');
+    const target = image.getBoundingClientRect();
+    swipelist.classList.add('overlaid');
+    details.classList.remove('hidden');
+
+    item.style.overflow = 'visible';
+    carousel.style.transformOrigin = 'top left';
+    carousel.style.transition = 'transform 0.15s ease-in-out';
+    return requestAnimationFramePromise()
+      .then(_ => requestAnimationFramePromise())
+      .then(_ => {
+        carousel.style.transform = `scaleX(${target.width/start.width}) scaleY(${target.height/start.height}) translate(${target.left - start.left}px, ${target.top - start.top}px)`;   
+        detailsText1.style.transform = '';
+        detailsText2.style.transform = '';
+        detailsNav.style.transform = '';
+        return transitionEndPromise(carousel);
+      })
+      .then(_ => {
+        carousel.style.transform = '';
+        carousel.style.transition = '';
+        carousel.style.transformOrigin = '';
+        item.style.overflow = 'hidden';
+        details.classList.add('hidden');
+        swipelist.classList.remove('overlaid');
+      });
+  }
+
+  function copyControls() {
+    document.querySelectorAll('.view--details .control').forEach(btn => {
+      const actionName = Array.from(btn.classList).find(name => /(like|nope)/.test(name));
+      const svg = document.querySelector(`.view--swipelist .${actionName} svg`).cloneNode(true);
+      btn.appendChild(svg);
+    });
+  }
+
+  function installServiceWorker() {
+    if (!('serviceWorker' in navigator)) return;
+    navigator.serviceWorker.register('sw.js');
+  }
+
+  function init() {
+    const top = document.querySelector('.item--top');
+    top.data = dataProvider.next().value;
+    top.addEventListener('swipe', updateCards);
+    top.addEventListener('details', showDetails);
+    const next = document.querySelector('.item--next');
+    next.data = dataProvider.next().value;
+    const details = document.querySelector('tinderforbananas-details');
+    details.addEventListener('dismiss', hideDetails);
+    copyControls();
+    adjustSwipeItems();
+    window.addEventListener('resize', adjustSwipeItems);
+    hookupButtons();
+    installServiceWorker();
+  }
+  document.addEventListener('DOMContentLoaded', init);
+})();
